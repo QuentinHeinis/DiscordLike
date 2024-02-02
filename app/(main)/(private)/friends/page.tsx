b3247e47955be7a31447b06dafb5899d7663c7ec
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-02 13:59:19 +0100

    feat : remove logout btn from friends

--- app/(main)/(private)/friends/page.tsx
+++ app/(main)/(private)/friends/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 import ChannelHeader from "@/components/navigation/ChannelHeader"
-import MessageInput from "@/components/ui/MessageInput"
-import MessageZone from "@/components/ui/MessageZone"
-import { signOut } from "next-auth/react"
-
 
 const MessagesPage = () => {
-  
+
   return (
     <main className="max-h-screen h-screen bg-slate-700 pt-20 w-full p-2 pb-0 relative">
       <ChannelHeader isMsg={false} title='Amis'/>
-      <div onClick={()=>signOut()}>logout</div>
     </main>
   )
 }
