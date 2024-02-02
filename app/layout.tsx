7a8f3b731d4fb9000128daf762769d13d60fee92
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-02 12:46:16 +0100

    feat : fix layout import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import ToasterContext from "./context/ToasterContext";
-import AuthContext from "./context/authContext";
+import AuthContext from "./context/AuthContext";
 
 
 const inter = Inter({ subsets: ["latin"] });
