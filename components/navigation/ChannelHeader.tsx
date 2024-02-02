d89cbf0ae91638ba744ab594ebddc3e6014e4e98
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-02 15:11:42 +0100

    feat : hide menu icon on large size

--- components/navigation/ChannelHeader.tsx
+++ components/navigation/ChannelHeader.tsx
@@ -8,7 +8,7 @@ const ChannelHeader = ({title, isMsg}: {title:string, isMsg:boolean}) => {
   }
   return (
     <div className="h-16 w-full bg-slate-800 absolute top-0 left-0 right-0 flex items-center gap-3 px-3 border-b border-slate-200">
-      <button onClick={()=>toggleMenu()}><Bars3Icon className='h-6 w-6 text-white'/></button>
+      <button onClick={()=>toggleMenu()} className="md:hidden"><Bars3Icon className='h-6 w-6 text-white'/></button>
       {isMsg ? <ChatBubbleLeftRightIcon className='h-6 w-6 text-white'/> : ''}
       {title}
     </div>
