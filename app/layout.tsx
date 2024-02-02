af19af17abb0fcbc64b37a0030d4c21042753a74
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-02 11:36:29 +0100

    feat : add toaster context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import ToasterContext from "./context/ToasterContext";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={`flex ${inter.className}`}>
+        <ToasterContext/>
         {children}
       </body>
     </html>
