71842e8e5c77c29affbe5033da0fdfd228af4507
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-02 11:36:46 +0100

    feat : add btn and input component

---
+++ components/form/Button.tsx
@@ -0,0 +1,44 @@
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type = "button",
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
+  return ( 
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={`
+        bg-red-500
+        flex 
+        justify-center 
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `}
+    >
+      {children}
+    </button>
+   );
+}
+ 
+export default Button;
