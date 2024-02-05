70ca7eb706c636d8e88c935c4f6bb2e396c1c375
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-05 09:58:12 +0100

    feat : add settings base style

--- app/(main)/(private)/settings/page.tsx
+++ app/(main)/(private)/settings/page.tsx
@@ -1,6 +1,9 @@
 const page = () => {
   return (
-    <div>page</div>
+    <main className="max-h-screen h-screen bg-slate-700 w-full p-2 pb-0 relative">
+      <h1>Settings</h1>
+
+    </main>
   )
 }
 
