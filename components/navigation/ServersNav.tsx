78975424ebb618e59815eea5936dcecace3c1752
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-06 21:51:42 +0100

    feat : copy invite link

--- components/navigation/ServersNav.tsx
+++ components/navigation/ServersNav.tsx
@@ -19,7 +19,7 @@ const isModo = checkModo ? true : false
 
   return (
     <div>
-      <ServerHeader serverName={server.name}/>
+      <ServerHeader serverName={server.name} inviteCode={server.inviteCode}/>
       <ServerChannel channels={server.channels} serverId={server.id} isModo={isModo}/>
       <ServerMember members={server.members}/>
     </div>
--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { ChevronDownIcon, UserPlusIcon } from "@heroicons/react/24/solid"
+import toast from "react-hot-toast"
 
 
 type ServerUtilsType = {
@@ -13,10 +15,19 @@ const ServerUtils = ({text, icon} : ServerUtilsType) => {
 
 
 type ServerHeaderType = {
-  serverName: string
+  serverName: string,
+  inviteCode:string
 }
 
-const ServerHeader = ({serverName}:ServerHeaderType) => {
+const handleCopy = (code:string) => {
+  console.log('copied')
+  // copy on clipboard
+  const inviteLink = 'https://suihira.heinis.dev/invite/' + code
+  navigator.clipboard.writeText(inviteLink)
+  toast.success('Lien copié dans le presse-papier')
+}
+
+const ServerHeader = ({serverName, inviteCode}:ServerHeaderType) => {
   return (
       <div className='group relative w-full bg-slate-500 flex justify-around items-center h-16 border-b border-slate-200'>
         <p className='text-xl font-bold text-slate-100'>{serverName}</p>
@@ -24,7 +35,7 @@ const ServerHeader = ({serverName}:ServerHeaderType) => {
           <ChevronDownIcon className='group-hover:rotate-180 transition-all h-6 w-6'/>
         </div>
         <div className='h-0 group-hover:h-fit transition-all scale-50 group-hover:scale-100 overflow-hidden absolute w-[90%] bg-slate-500 top-full'>
-          <ServerUtils text='Inviter des gens' icon={<UserPlusIcon className='h-6'/>}/>
+          <button onClick={()=>handleCopy(inviteCode)}><ServerUtils text='Inviter des gens' icon={<UserPlusIcon className='h-6'/>}/></button>
           <ServerUtils text='Paramètres du serveur' icon={<UserPlusIcon className='h-6'/>}/>
           <ServerUtils text='Créer un salon' icon={<UserPlusIcon className='h-6'/>}/>
         </div>
