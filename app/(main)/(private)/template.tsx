ae77e4351410d938772d1779d9aac1cad1af6a13
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-06 20:33:35 +0100

    remove useless template

--- app/(main)/(private)/template.tsx
+++
@@ -1,9 +0,0 @@
-import getCurrentUser from "@/lib/current-profil"
-
-export default async function Template({ children }: { children: React.ReactNode }) {  
-  return (
-  <>
-    {children}
-  </>
-  )
-}
