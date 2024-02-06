a630674dd67b563636666d69a7dac3cedbf44d44
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-06 21:31:23 +0100

    feat : handle multiLine

--- components/chat/ChatItem.tsx
+++ components/chat/ChatItem.tsx
@@ -175,7 +175,7 @@ export const ChatItem = ({
             </div>
           )}
           {!fileUrl && !isEditing && (
-            <p className="text-sm text-zinc-600 dark:text-zinc-300">
+            <p className="text-sm whitespace-pre-line text-zinc-600 dark:text-zinc-300">
               {content}
               {isUpdated && !deleted && (
                 <span className="text-[10px] mx-2 text-zinc-500 dark:text-zinc-400">
