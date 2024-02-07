d1ca85b338a962e3271b8d51f9270662c3f5c198
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-07 22:10:40 +0100

    feat : server form : change form

--- components/form/modalForm/CreateServerForm.tsx
+++ components/form/modalForm/CreateServerForm.tsx
@@ -8,6 +8,7 @@ import Image from "next/image"
 import { redirect, useRouter } from "next/navigation"
 import { useStore } from "@/store/zustand"
 import toast from "react-hot-toast"
+import { PencilIcon } from "@heroicons/react/24/solid"
 
 const AddServer = () => {
   const { register, handleSubmit, formState: { errors }, setValue, watch } = useForm<FieldValues>()
@@ -36,13 +37,27 @@ const AddServer = () => {
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)}  className="flex flex-col gap-5 items-center">
-        {image && <Image src={image} alt="server image" width={100} height={100} />}
         <CldUploadButton 
           options={{ maxFiles: 1 }} 
           onUpload={handleUpload} 
           uploadPreset="jgueeeco"
-        >
-          Ajouter une image
+          className="rounded-full overflow-hidden"
+          >
+          <div className="group relative">
+            {
+              image && <Image src={image} alt="server image" width={100} height={100} />
+            }
+            {
+              !image  && 'Ajouter une image'
+            }
+            {
+              image  && (
+                <div className="group-hover:visible invisible absolute top-0 right-0 left-0 bottom-0  bg-black bg-opacity-50 grid place-content-center">
+                  <PencilIcon className="h-10 w-10"/>
+                </div>
+              )
+            }
+          </div>
         </CldUploadButton>
         <Input
           register={register}
