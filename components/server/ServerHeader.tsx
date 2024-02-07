39a3a49b50aa17a3ed78e15709f00ce8539fe58e
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2024-02-07 20:13:42 +0100

    feat : adjust zindex on serverheader

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -42,7 +42,7 @@ const ServerHeader = ({serverName, inviteCode, server, isModo, isOwner}:ServerHe
         <div>
           <ChevronDownIcon className='group-hover:rotate-180 transition-all h-6 w-6'/>
         </div>
-        <div className='h-0 group-hover:h-fit group-hover:visible pt-2 pb-2 transition-all scale-50 invisible flex gap-1 flex-col items-center group-hover:scale-100 overflow-hidden absolute w-[95%] bg-slate-500 top-full border-t'>
+        <div className='h-0 z-50 group-hover:h-fit group-hover:visible pt-2 pb-2 transition-all scale-50 invisible flex gap-1 flex-col items-center group-hover:scale-100 overflow-hidden absolute w-[95%] bg-slate-500 top-full border-t'>
           <button className="w-5/6" onClick={()=>handleCopy(inviteCode)}><ServerUtils text='Inviter des gens' icon={<ClipboardIcon className='h-4'/>}/></button>
           {
             isModo && 
