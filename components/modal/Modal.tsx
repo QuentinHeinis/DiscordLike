74ddb94a14221d2116f06f1eeadbb84966523af0
Quentin Heinis <quentin@heinis.dev>
2025-01-19 21:50:15 +0100

    feat(updateServerForm) : add data on toggle + fix modal state

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,95 +1,99 @@
-'use client'
-import { XMarkIcon } from "@heroicons/react/24/solid"
-import { useStore } from "@/store/zustand"
-import CreateServerForm from "../form/modalForm/CreateServerForm"
-import CreateChannel from "../form/modalForm/CreateChannelForm"
-import { useEffect, useState } from "react"
-import UpdateChannel from "../form/modalForm/UpdateChannelForm"
-import { DeleteMessageModal } from "../form/modalForm/DeleteMessageForm"
-import UpdateServer from "../form/modalForm/UpdateServerForm"
-import UpdateProfil from "../form/modalForm/UpdateProfilForm"
-import { LeaveServer } from "../form/modalForm/LeaveServerForm"
-import UpdateMember from "../form/modalForm/UpdateMemberForm"
+"use client";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useStore } from "@/store/zustand";
+import CreateServerForm from "../form/modalForm/CreateServerForm";
+import CreateChannel from "../form/modalForm/CreateChannelForm";
+import { useEffect, useState } from "react";
+import UpdateChannel from "../form/modalForm/UpdateChannelForm";
+import { DeleteMessageModal } from "../form/modalForm/DeleteMessageForm";
+import UpdateServer from "../form/modalForm/UpdateServerForm";
+import UpdateProfil from "../form/modalForm/UpdateProfilForm";
+import { LeaveServer } from "../form/modalForm/LeaveServerForm";
+import UpdateMember from "../form/modalForm/UpdateMemberForm";
 
 const Modal = () => {
-  const {modalOpen, setModalOpen} = useStore()
-  const [name, setName] = useState<string>('Create a server')
+  const { modalOpen, setModalOpen } = useStore();
+  const [name, setName] = useState<string>("Create a server");
 
   useEffect(() => {
     switch (modalOpen) {
-      case 'none':
-        return 
-      case 'addServer':
-        setName('Créer un serveur')
-        break
-      case 'addChannel':
-        setName('Créer un channel')
-        break
-      case 'updateChannel':
-        setName('Modifier un channel')
-        break
-      case 'deleteMessage':
-        setName('Supprimer un message')
-        break
-      case 'updateServer':
-        setName('Modifier le serveur')
-      case 'updateProfile':
-        setName('Modifier le profil')
-        break
-      case 'leaveServer':
-        setName('Quitter le serveur')
-      case 'updateMember':
-        setName('Modifier le membre')
-        break
+      case "none":
+        return;
+      case "addServer":
+        setName("Créer un serveur");
+        break;
+      case "addChannel":
+        setName("Créer un channel");
+        break;
+      case "updateChannel":
+        setName("Modifier un channel");
+        break;
+      case "deleteMessage":
+        setName("Supprimer un message");
+        break;
+      case "updateServer":
+        setName("Modifier le serveur");
+        break;
+      case "updateProfile":
+        setName("Modifier le profil");
+        break;
+      case "leaveServer":
+        setName("Quitter le serveur");
+        break;
+      case "updateMember":
+        setName("Modifier le membre");
+        break;
       default:
-        return
+        return;
     }
-  }, [modalOpen])
-
+  }, [modalOpen]);
 
   const RenderModal = () => {
     switch (modalOpen) {
-      case 'none':
-        return 
-      case 'addServer':
-        return <CreateServerForm/>
-      case 'addChannel':
-        return <CreateChannel/>
-      case 'updateChannel':
-        return <UpdateChannel/>
-      case 'deleteMessage':
-        return <DeleteMessageModal/>
-      case 'updateServer':
-        return <UpdateServer/>
-      case 'updateProfile':
-        return <UpdateProfil/>
-      case 'leaveServer':
-        return <LeaveServer/>
-      case 'updateMember':
-        return <UpdateMember/>
+      case "none":
+        return;
+      case "addServer":
+        return <CreateServerForm />;
+      case "addChannel":
+        return <CreateChannel />;
+      case "updateChannel":
+        return <UpdateChannel />;
+      case "deleteMessage":
+        return <DeleteMessageModal />;
+      case "updateServer":
+        return <UpdateServer />;
+      case "updateProfile":
+        return <UpdateProfil />;
+      case "leaveServer":
+        return <LeaveServer />;
+      case "updateMember":
+        return <UpdateMember />;
       default:
-        return
+        return;
     }
-  }
+  };
 
   return (
     <>
-    {
-      modalOpen !== 'none' &&
-      <div className="fixed text-white top-0 left-0 w-full h-full bg-neutral-800 bg-opacity-50 z-50 flex items-center justify-center">
-        
-        <div className="bg-neutral-700 p-4 rounded-lg w-96">
-          <div className="flex justify-between items-center">
-            <h3 className="text-lg font-semibold">{name}</h3>
-            <button onClick={()=>setModalOpen('none')}><XMarkIcon className="h-6 w-6 text-white"/></button>
+      {modalOpen !== "none" && (
+        <div className="fixed text-white top-0 left-0 w-full h-full bg-neutral-800 bg-opacity-50 z-50 flex items-center justify-center">
+          <div className="bg-neutral-700 p-4 rounded-lg w-96">
+            <div className="flex justify-between items-center">
+              <h3 className="text-lg font-semibold">{name}</h3>
+              <button onClick={() => setModalOpen("none")}>
+                <XMarkIcon className="h-6 w-6 text-white" />
+              </button>
+            </div>
+            <RenderModal />
           </div>
-          <RenderModal/>
+          <span
+            onClick={() => setModalOpen("none")}
+            className="absolute -z-10 top-0 right-0 bottom-0 left-0"
+          ></span>
         </div>
-        <span onClick={()=>setModalOpen('none')} className="absolute -z-10 top-0 right-0 bottom-0 left-0"></span>
-      </div>
-    }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -1,62 +1,107 @@
-'use client'
-import { useStore } from "@/store/zustand"
-import { ChevronDownIcon, ClipboardIcon, Cog6ToothIcon, PlusCircleIcon, UserPlusIcon } from "@heroicons/react/24/solid"
-import { LeaveIcon } from "@livekit/components-react"
-import { Server } from "@prisma/client"
-import Link from "next/link"
-import toast from "react-hot-toast"
-
+"use client";
+import { useStore } from "@/store/zustand";
+import {
+  ChevronDownIcon,
+  ClipboardIcon,
+  Cog6ToothIcon,
+  PlusCircleIcon,
+  UserPlusIcon,
+} from "@heroicons/react/24/solid";
+import { LeaveIcon } from "@livekit/components-react";
+import { Server } from "@prisma/client";
+import Link from "next/link";
+import toast from "react-hot-toast";
 
 type ServerUtilsType = {
-  text: string,
-  icon: JSX.Element
-}
-const ServerUtils = ({text, icon} : ServerUtilsType) => {
-    return(
-      <p className='flex items-center gap-2'>{icon} {text}</p>
-    )
-  }
-
+  text: string;
+  icon: JSX.Element;
+};
+const ServerUtils = ({ text, icon }: ServerUtilsType) => {
+  return (
+    <p className="flex items-center gap-2">
+      {icon} {text}
+    </p>
+  );
+};
 
 type ServerHeaderType = {
-  serverName: string,
-  inviteCode:string,
-  server : Server,
-  isModo: boolean,
-  isOwner: boolean
-}
+  serverName: string;
+  inviteCode: string;
+  server: Server;
+  isModo: boolean;
+  isOwner: boolean;
+};
 
-const handleCopy = (code:string) => {
-  console.log('copied')
+const handleCopy = (code: string) => {
+  console.log("copied");
   // copy on clipboard
-  const inviteLink = 'https://suihira.heinis.dev/invite/' + code
-  navigator.clipboard.writeText(inviteLink)
-  toast.success('Lien copié dans le presse-papier')
-}
+  const inviteLink = "https://suihira.heinis.dev/invite/" + code;
+  navigator.clipboard.writeText(inviteLink);
+  toast.success("Lien copié dans le presse-papier");
+};
 
-const ServerHeader = ({serverName, inviteCode, server, isModo, isOwner}:ServerHeaderType) => {
-  const {setModalOpen} = useStore()
+const ServerHeader = ({
+  serverName,
+  inviteCode,
+  server,
+  isModo,
+  isOwner,
+}: ServerHeaderType) => {
+  const { setModalOpen } = useStore();
   return (
-      <div className='group flex-none relative w-full bg-neutral-600 flex justify-around items-center h-16 border-b border-neutral-200'>
-        <p className='text-xl font-bold text-neutral-100'>{serverName}</p>
-        <div>
-          <ChevronDownIcon className='group-hover:rotate-180 transition-all h-6 w-6'/>
-        </div>
-        <div className='h-0 z-50 group-hover:h-fit group-hover:visible pt-2 pb-2 transition-all scale-50 invisible flex gap-1 flex-col items-center group-hover:scale-100 overflow-hidden absolute w-[95%] bg-neutral-500 top-full border-t'>
-          <button className="w-5/6" onClick={()=>handleCopy(inviteCode)}><ServerUtils text='Inviter des gens' icon={<ClipboardIcon className='h-4'/>}/></button>
-          {
-            isModo && 
-            <>
-              <button className="w-5/6" onClick={()=>setModalOpen("updateServer", {server:server})}><ServerUtils text='Paramètres du serveur' icon={<Cog6ToothIcon className='h-4'/>}/></button>
-              <button className="w-5/6" onClick={()=>setModalOpen("addChannel")}><ServerUtils text='Créer un salon' icon={<PlusCircleIcon className='h-4'/>}/></button>
-            </>
-          }
-          {
-            isOwner || <button className="w-5/6 text-red-500" onClick={()=>setModalOpen("leaveServer", {server:server})}><ServerUtils text='Quitter le server' icon={<LeaveIcon className='h-4'/>}/></button>
-          }
-        </div>
+    <div className="group flex-none relative w-full bg-neutral-600 flex justify-around items-center h-16 border-b border-neutral-200">
+      <p className="text-xl font-bold text-neutral-100">{serverName}</p>
+      <div>
+        <ChevronDownIcon className="group-hover:rotate-180 transition-all h-6 w-6" />
       </div>
-  )
-}
+      <div className="h-0 z-50 group-hover:h-fit group-hover:visible pt-2 pb-2 transition-all scale-50 invisible flex gap-1 flex-col items-center group-hover:scale-100 overflow-hidden absolute w-[95%] bg-neutral-500 top-full border-t">
+        <button className="w-5/6" onClick={() => handleCopy(inviteCode)}>
+          <ServerUtils
+            text="Inviter des gens"
+            icon={<ClipboardIcon className="h-4" />}
+          />
+        </button>
+        {isModo && (
+          <>
+            <button
+              className="w-5/6"
+              onClick={() =>
+                setModalOpen("updateServer", {
+                  server: server,
+                  other: { name: serverName },
+                })
+              }
+            >
+              <ServerUtils
+                text="Paramètres du serveur"
+                icon={<Cog6ToothIcon className="h-4" />}
+              />
+            </button>
+            <button
+              className="w-5/6"
+              onClick={() => setModalOpen("addChannel")}
+            >
+              <ServerUtils
+                text="Créer un salon"
+                icon={<PlusCircleIcon className="h-4" />}
+              />
+            </button>
+          </>
+        )}
+        {isOwner || (
+          <button
+            className="w-5/6 text-red-500"
+            onClick={() => setModalOpen("leaveServer", { server: server })}
+          >
+            <ServerUtils
+              text="Quitter le server"
+              icon={<LeaveIcon className="h-4" />}
+            />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default ServerHeader
+export default ServerHeader;
